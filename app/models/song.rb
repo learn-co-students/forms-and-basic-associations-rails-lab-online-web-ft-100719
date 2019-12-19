48aa156af2ac05ae438e02c278fd2270ecea58d4
Emily <emily.a.harber@gmail.com>
2019-12-19 14:52:35 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,17 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  accepts_nested_attributes_for :genre
+  has_many :notes
+  accepts_nested_attributes_for :notes
+  
+  def artist_name=(artist) 
+    self.artist = Artist.find_or_create_by(name: artist)
+  end 
+
+  def artist_name 
+    self.artist ? self.artist.name : nil
+  end 
+
 end
