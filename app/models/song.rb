30f4ac601e79894df9e0ed24364ed139fb8d0cbc
Victoria Hale <vic.hale74@gmail.com>
2019-12-19 13:04:26 -0800

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,15 @@
 class Song < ActiveRecord::Base
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
   # add associations here
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end 
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
 end
